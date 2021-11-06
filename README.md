24e9c8ce5a662fd137b0258b99a0a30e66789ef4
andrew <asteel22@outlook.com>
2021-11-06 12:38:42 -0400

    Added Users model with bcrypt, view + partials in ejs, began form action for user authentication

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A simple Authorization Application made with Node.js, Express.js, and MongoDB. Front-end pending :)
+A simple Authorization Application made with Node.js, Express.js, MongoDB, and Bootstrap. Front-end pending :)
--- app.js
+++ app.js
@@ -21,43 +21,33 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Demo Auth App');
+// view engine setup
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(port);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
-server.listen(port, hostname, () => {
-  console.log('Server running at http://' +hostname+ ':'+port);
+app.use((req, res) => {
+  res.status(404).render('404');
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+/* Delete?
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+*/
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
